24950c643ea3e76266794e616d2bcf5659e73d4d
Noé <mace.noe@gmail.com>
2024-03-10 13:24:45 +0000

    Sponsored transactions work on testn'et

--- backend/sponsored.py
+++ backend/sponsored.py
@@ -7,13 +7,13 @@ import os
 # Environment variable for the private key
 network = 'testnet'
 
-wallet = wallet_create_or_open("Sponsorising", network= network, witness_type='P2TR')
+wallet = wallet_create_or_open("Bitso", network= network, witness_type='segwit')
 print(wallet.scan())
 print(wallet.info())
 
 private_key = wallet.get_key()
-print("wifazepzajeozairjhazoirazoraz")
-print(wallet.wif())
+#print("wifazepzajeozairjhazoirazoraz")
+#print(wallet.wif())
 account = wallet.new_account("Sponsor")
 
 
@@ -29,18 +29,20 @@ def mint():
         return jsonify({"status": "error", "message": "Invalid user address or amount"}), 400
 
     try:
-        # Initialize wallet
-        print(wallet.scan())
-        print(wallet.info())
-        print(wallet.get_key())
-
-        # Create, sign, and send the transaction in one step
-        # Replace `send_to` with the actual function name and its required parameters
-        txid = wallet.send_to(user_address, amount, network = network)
-
-        return jsonify({"status": "success", "transaction_id": txid}), 200
+        tx = wallet.send_to(user_address, amount, network=network, offline=False)
+        print(tx)
+        wallet.scan()
+        #t = wallet.sweep(user_address, network = network)
+        print(t)
+        # Extract relevant transaction information
+        print(t.outputs)
+        tx_data = {
+            "txid": tx.txid,
+            "amount": amount,
+            "user_address": user_address
+        }
+        return jsonify({"status": "success", "transaction": tx_data}), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
-
 if __name__ == '__main__':
     app.run(debug=True)
